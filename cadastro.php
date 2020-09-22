a20d45e32bf9cbd98c78f0ba470ad844d8d518f3
Pedro Leite <pedrodrop.21@gmail.com>
2020-09-22 10:16:17 -0300

    att php

---
+++ cadastro.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'config.php';
+require 'Usuario.class.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <link href="css/style.css" rel="stylesheet">
+        <title>Menu</title>
+</head>
+<body>
+<h1 class="tit">Cadastro</h1>
+<div class="cadastro">
+
+        <form method="post" action="cadastrado.php">
+
+                <p>
+                  <label for="nome">Nome:</label><br>
+                  <input id="nome" name="nome" required="required" type="text" placeholder="Nome" />
+                </p>
+                <p>
+                        <label for="cpf">CPF/CNPJ:</label><br>
+                        <input id="cpf" name="cpf" required="required" type="text" placeholder="999 999 999 99"/>
+                  </p>
+
+                <p>
+                  <label for="email">e-mail:</label><br>
+                  <input id="email" name="email" required="required" type="email" placeholder="contato@htmlecsspro.com"/>
+                </p>
+
+                <p>
+                  <label for="nascimento">Data de nascimento:</label><br>
+                  <input id="nascimento" name="nascimento" required="required" type="date" placeholder="01/01/2000"/>
+                </p>
+                <p>
+                        <label for="telefone">Telefone:</label><br>
+                        <input id="telefone" name="telefone" required="required" type="text" placeholder="99999 9999" />
+                </p>
+                  <p>
+                        <label for="senha">Senha:</label><br>
+                        <input id="senha" name="senha" required="required" type="password" placeholder="senha" /><br>
+                        <input id="senha" name="senha" required="required" type="password" placeholder="Confirmar senha" />
+                </p>
+                <p>
+                  <input  type="submit" value="Cadastrar"/>
+                </p>
+</div>
+</body>
+</html>
---
+++ menuInicial.php
@@ -0,0 +1,94 @@
+<?php
+
+if(isset($_POST['email'])&& !empty($_POST['email']) &&isset($_POST['senha'])&& !empty($_POST['senha'])){
+        require 'config.php';
+        require 'Usuario.class.php';
+
+        $u = new Usuario();
+
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+
+        if($u->login($email,$senha) == true){
+                if(isset($_SESSION['email'])){
+                        header ("Location menuInicial.php");
+                }
+        }else{
+                echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='index.php';</script>";
+
+        }
+
+        }else{
+                header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <link href="css/style.css" rel="stylesheet">
+        <title>Menu</title>
+</head>
+
+<body>
+
+        <nav class="menu" id="principal">
+                <ul>
+                        <h1 align="center">GERFIN</h1>
+
+                        <li><a href="">Início</a></li>
+                        <li><a href="#cliente">Cliente<span>+</span></a></i></li>
+                        <li><a href="#Contasapagar">Contas a pagar<span>+</span></a></li>
+                        <li><a href="#ContasaReceber">Contas a Receber<span>+</span></a></li>
+                        <li><a href="#Relatorios">Relatórios<span>+</span></a></li>
+                        <li><a href="#Agenda">Agenda <span>+</span></a></li>
+                </ul>
+                </nav>
+        </nav>
+
+                <nav class="menu" id="cliente">
+                <ul>
+                        <li><a href="" class="voltar">Voltar</a></li>
+                        <li><a href="">Novo</a></li>
+                        <li><a href="">Consulta</a></li>
+                </ul>
+        </nav>
+
+        <nav class="menu" id="Contasapagar">
+                <ul>
+                        <li><a href="" class="voltar">Voltar</a></li>
+                        <li><a href="">Boletos</a></li>
+                        <li><a href="">Prolabore</a></li>
+                        <li><a href="">Salário</a></li>
+                        <li><a href="">Bolsas</a></li>
+                        <li><a href="">Distribuição de Lucros</a></li>
+                </ul>
+        </nav>
+
+
+        <nav class="menu" id="ContasaReceber">
+                <ul>
+                        <li><a href="" class="voltar">Voltar</a></li>
+                        <li><a href="">Honorários De Prolabore</a></li>
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+
+        if($u->login($email,$senha) == true){
+                if(isset($_SESSION['email'])){
+                        header ("Location menuInicial.php");
+                }
+        }else{
+                echo"<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='index.php';</script>";
+
+        }
+
+        }else{
+                header("Location: index.php");
+}
+
+?>
